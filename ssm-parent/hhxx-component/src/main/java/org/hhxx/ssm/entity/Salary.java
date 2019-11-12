34cdc26d84c82d18aa2b25cde41e598a2c92a972
wx85107403 <398360726@qq.com>
2019-11-12 17:55:28 +0800

    salary实体类

---
+++ ssm-parent/hhxx-component/src/main/java/org/hhxx/ssm/entity/Salary.java
@@ -0,0 +1,102 @@
+package org.hhxx.ssm.entity;
+
+public class Salary {
+
+	private Integer id;
+	private Integer salaryYear;
+	private Integer salaryMonth;
+	private Double baseSalary;
+	private Double insurance;
+	private Double accumulationFund;
+	private Double reallySalary;
+	private Integer empId;
+
+	public Salary() {
+		super();
+	}
+
+	public Salary(Integer id, Integer salaryYear, Integer salaryMonth, Double baseSalary, Double insurance,
+			Double accumulationFund, Double reallySalary, Integer empId) {
+		super();
+		this.id = id;
+		this.salaryYear = salaryYear;
+		this.salaryMonth = salaryMonth;
+		this.baseSalary = baseSalary;
+		this.insurance = insurance;
+		this.accumulationFund = accumulationFund;
+		this.reallySalary = reallySalary;
+		this.empId = empId;
+	}
+
+	@Override
+	public String toString() {
+		return "Salary [id=" + id + ", salaryYear=" + salaryYear + ", salaryMonth=" + salaryMonth + ", baseSalary="
+				+ baseSalary + ", insurance=" + insurance + ", accumulationFund=" + accumulationFund + ", reallySalary="
+				+ reallySalary + ", empId=" + empId + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getSalaryYear() {
+		return salaryYear;
+	}
+
+	public void setSalaryYear(Integer salaryYear) {
+		this.salaryYear = salaryYear;
+	}
+
+	public Integer getSalaryMonth() {
+		return salaryMonth;
+	}
+
+	public void setSalaryMonth(Integer salaryMonth) {
+		this.salaryMonth = salaryMonth;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Double getInsurance() {
+		return insurance;
+	}
+
+	public void setInsurance(Double insurance) {
+		this.insurance = insurance;
+	}
+
+	public Double getAccumulationFund() {
+		return accumulationFund;
+	}
+
+	public void setAccumulationFund(Double accumulationFund) {
+		this.accumulationFund = accumulationFund;
+	}
+
+	public Double getReallySalary() {
+		return reallySalary;
+	}
+
+	public void setReallySalary(Double reallySalary) {
+		this.reallySalary = reallySalary;
+	}
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+}
